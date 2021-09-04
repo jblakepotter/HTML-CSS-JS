3ea19314cbf81b0123241b4df2b4173a0f506cd5
Jordan <76089959+jblakepotter@users.noreply.github.com>
2021-09-04 14:32:17 -0700

    Create speakhello.js

---
+++ mod4-solution/speakhello.js
@@ -0,0 +1,10 @@
+(function (window) {
+  var helloSpeaker = {};
+
+  var speakWord = "Hello"; 
+
+  helloSpeaker.speak = function (names) {
+    console.log(speakWord + " " + names);
+} ;window.helloSpeaker = helloSpeaker;
+
+})(window)
